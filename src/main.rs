7629a56d8761b61d119c614510c5304a8fa1f387
Broderick Carlin <broderick.carlin@gmail.com>
2024-12-10 19:50:39 -0600

    chore: Update embassy to latest

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 #![no_std]
 #![no_main]
-#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
 
-use defmt::info;
+use defmt::*;
 use embassy_executor::Spawner;
-use {defmt_rtt as _, panic_probe as _}; // global logger
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let _p = embassy_stm32::init(Default::default());
+    let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
+
+    let mut led = Output::new(p.PA5, Level::High, Speed::Low);
+
+    loop {
+        info!("high");
+        led.set_high();
+        Timer::after_millis(1000).await;
+
+        info!("low");
+        led.set_low();
+        Timer::after_millis(1000).await;
+    }
 }
